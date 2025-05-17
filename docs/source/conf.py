5fb95570b150d0090aaec2dcb6a7fd401a51368d
Amit Gupta <amit_@outlook.in>
2025-05-17 04:08:31 -0500

    fix todo erros

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,15 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+import sys, types
+MOCKS = [
+    "pytorch_lightning",
+    "torch_scatter",
+    "torch_sparse",
+]
+for name in MOCKS:
+    sys.modules[name] = types.ModuleType(name)
+
 import os
 import sys
 import subprocess
